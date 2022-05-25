237310ccb05a2ee56490b9e628a02b4d7cf98653
Suman Mondal <suman.mandal1020@gmail.com>
2022-05-25 18:15:08 +0530

    Create Job Feed component. Fixes #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import { useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { auth } from './firebase';
+import { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import { auth } from "./firebase";
 import {
   ActionPage,
   LoginPage,
   ResetPage,
   SignupPage,
-} from './pages/authentication';
-import { DashboardPage } from './pages/dashboard/DashboardPage';
-import { HomePage } from './pages/dashboard/HomePage';
-import { NoMatchPage } from './pages/NoMatchPage';
-import { VerifyUserPage } from './pages/verification/VerifyUserPage';
-import { useAppDispatch } from './_state/hooks';
-import { login, logout } from './_state/slices/auth';
+} from "./pages/authentication";
+import { DashboardPage } from "./pages/dashboard/DashboardPage";
+import { HomePage } from "./pages/dashboard/HomePage";
+import { JobFeedPage } from "./pages/dashboard/JobFeedPage";
+import { NoMatchPage } from "./pages/NoMatchPage";
+import { VerifyUserPage } from "./pages/verification/VerifyUserPage";
+import { useAppDispatch } from "./_state/hooks";
+import { login, logout } from "./_state/slices/auth";
 
 function App() {
   const dispatch = useAppDispatch();
@@ -28,18 +29,19 @@ function App() {
 
   return (
     <Routes>
-      <Route path='/' element={<DashboardPage />}>
+      <Route path="/" element={<DashboardPage />}>
         <Route index element={<HomePage />} />
-        <Route path='verify' element={<VerifyUserPage />} />
+        <Route path="verify" element={<VerifyUserPage />} />
+        <Route path="job-feed" element={<JobFeedPage />} />
         {/* <Route path='discover' element={<DiscoverPage />} /> */}
         {/* <Route path='notifications' element={<NotificationPage />} /> */}
         {/* <Route path='profile' element={<ProfilePage />} /> */}
       </Route>
-      <Route path='login' element={<LoginPage />} />
-      <Route path='signup' element={<SignupPage />} />
-      <Route path='reset' element={<ResetPage />} />
-      <Route path='__action' element={<ActionPage />} />
-      <Route path='*' element={<NoMatchPage />} />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="signup" element={<SignupPage />} />
+      <Route path="reset" element={<ResetPage />} />
+      <Route path="__action" element={<ActionPage />} />
+      <Route path="*" element={<NoMatchPage />} />
     </Routes>
   );
 }
---
+++ src/pages/dashboard/JobFeedPage.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import {
+  chakra,
+  Button,
+  ButtonGroup,
+  Stack,
+  HStack,
+  VStack,
+  Box,
+  Flex,
+  useColorModeValue,
+  Heading,
+  Text,
+  Grid,
+  GridItem,
+  Tag,
+  TagLabel,
+  StackDivider,
+} from "@chakra-ui/react";
+
+// import { MdHeadset, MdEmail, MdLocationOn } from "react-icons/md";
+// import { BsFillBriefcaseFill } from "react-icons/bs";
+
+export default function JobFeedPage() {
+  return (
+    <Grid templateColumns="repeat(2, 1fr)" gap="4">
+      <GridItem>
+        <JobFeedItem />
+      </GridItem>
+    </Grid>
+  );
+}
+
+// type JobFeedItemProps = {
+//   title: string;
+//   company: string;
+//   location: string;
+//   mode: "Remote" | "Flexible" | "In-Office";
+//   offer: string;
+//   startdate: string;
+//   positions: number;
+//   deadline: string;
+// };
+
+function JobFeedItem() {
+  return (
+    <Box w="full" shadow="md" rounded="lg">
+      <VStack py={4} px={6} align="flex-start" spacing="2">
+        <Heading fontSize="2xl" color="gray.800">
+          Full Stack Developer
+        </Heading>
+        <Text pt="4" color="gray.600">
+          Tesla Motors India
+        </Text>
+        <HStack spacing={2}>
+          <Tag variant="outline" colorScheme="teal">
+            React Js
+          </Tag>
+          <Tag variant="outline" colorScheme="teal">
+            Express Js
+          </Tag>
+        </HStack>
+        <HStack spacing="6" fontSize="16">
+          {[
+            ["Offer", "6 LPA"],
+            ["Mode", "Remote"],
+            ["Start Date", "15 Aug 2022"],
+            ["Positions", "15"],
+            ["Deadline", "10 Sept 2022"],
+          ].map((item, key) => (
+            <VStack key={key} spacing="1" align="center" py="2">
+              <Text color="gray.500">{item[0]}</Text>
+              <Text color="blackAlpha.900">{item[1]}</Text>
+            </VStack>
+          ))}
+        </HStack>
+        <Stack direction="row" spacing={4} align="center">
+          <Button colorScheme="teal" variant="solid">
+            Apply Now
+          </Button>
+          <Button colorScheme="teal" variant="outline">
+            View Details
+          </Button>
+        </Stack>
+      </VStack>
+    </Box>
+  );
+}
