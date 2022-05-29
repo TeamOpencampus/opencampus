45e1dbc24749db6ff94ec26f58f98afed07ed6fe
Asim Bera <asimbera@outlook.in>
2022-05-29 11:59:18 +0530

    Implement basic details form in ProfileTab component. Fixes #13

--- src/pages/dashboard/SettingsPage.tsx
+++ src/pages/dashboard/SettingsPage.tsx
@@ -1,5 +1,20 @@
 import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  Button,
+  ButtonGroup,
+  FormControl,
+  FormHelperText,
+  FormLabel,
   Heading,
+  Input,
+  Select,
+  Stack,
+  StackDivider,
   Tab,
   TabList,
   TabPanel,
@@ -8,6 +23,7 @@ import {
   Text,
   VStack,
 } from '@chakra-ui/react';
+import React from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 export function SettingsPage() {
@@ -42,8 +58,128 @@ export function SettingsPage() {
   );
 }
 
+const PROFILE_SECTIONS: { title: string; element: React.ReactNode }[] = [
+  { title: 'Basic Details', element: <BasicDetailsForm /> },
+  { title: 'Academic Details', element: <AcademicDetailsForm /> },
+  { title: 'Work Experience', element: <WorkExperienceForm /> },
+  { title: 'Additional Documents', element: <AdditionalDocumentsForm /> },
+];
+
 function ProfileTab() {
-  return <Text>Profile</Text>;
+  return (
+    <Accordion defaultIndex={0}>
+      {PROFILE_SECTIONS.map((item, key) => (
+        <AccordionItem key={key}>
+          <AccordionButton>
+            <Box flex='1' textAlign='left'>
+              {item.title}
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+          <AccordionPanel>{item.element}</AccordionPanel>
+        </AccordionItem>
+      ))}
+    </Accordion>
+  );
+}
+
+function BasicDetailsForm() {
+  return (
+    <VStack align='flex-start' spacing='4' divider={<StackDivider />}>
+      {/* Full Name */}
+      <FormControl>
+        <Stack direction={['column', 'row']} spacing={['2', '20']}>
+          <Box w={['full', 'xs']}>
+            <FormLabel htmlFor='full-name'>Full Name</FormLabel>
+            <FormHelperText>Enter your full name.</FormHelperText>
+          </Box>
+          <Input id='full-name' width={['full', 'sm']} />
+        </Stack>
+      </FormControl>
+      {/* Email */}
+      <FormControl>
+        <Stack direction={['column', 'row']} spacing={['2', '20']}>
+          <Box w={['full', 'xs']}>
+            <FormLabel htmlFor='email'>Email Address</FormLabel>
+            <FormHelperText>
+              An email through the employer will contact you.
+            </FormHelperText>
+          </Box>
+          <Input id='email' width={['full', 'sm']} />
+        </Stack>
+      </FormControl>
+      {/* Phone Number */}
+      <FormControl>
+        <Stack direction={['column', 'row']} spacing={['2', '20']}>
+          <Box w={['full', 'xs']}>
+            <FormLabel htmlFor='phone'>Phone Number</FormLabel>
+            <FormHelperText>
+              An phone number through the employer will contact you.
+            </FormHelperText>
+          </Box>
+          <Input id='phone' width={['full', 'sm']} />
+        </Stack>
+      </FormControl>
+      {/* Gender */}
+      <FormControl>
+        <Stack direction={['column', 'row']} spacing={['2', '20']}>
+          <Box w={['full', 'xs']}>
+            <FormLabel htmlFor='gender'>Gender</FormLabel>
+            <FormHelperText>Lorem ipsum dolor sit amet.</FormHelperText>
+          </Box>
+          <Select
+            placeholder='Select Gender'
+            id='gender'
+            width={['full', 'sm']}
+          >
+            <option value='male'>Male</option>
+            <option value='female'>Female</option>
+            <option value='non_binary'>Non Binary</option>
+          </Select>
+        </Stack>
+      </FormControl>
+      {/* Nationality */}
+      <FormControl>
+        <Stack direction={['column', 'row']} spacing={['2', '20']}>
+          <Box w={['full', 'xs']}>
+            <FormLabel htmlFor='nationality'>Nationality</FormLabel>
+            <FormHelperText>Lorem ipsum dolor sit amet.</FormHelperText>
+          </Box>
+          <Input id='nationality' width={['full', 'sm']} />
+        </Stack>
+      </FormControl>
+      {/* Caste */}
+      <FormControl>
+        <Stack direction={['column', 'row']} spacing={['2', '20']}>
+          <Box w={['full', 'xs']}>
+            <FormLabel htmlFor='caste'>Caste</FormLabel>
+            <FormHelperText>Lorem ipsum dolor sit amet.</FormHelperText>
+          </Box>
+          <Select placeholder='Select Caste' id='caste' width={['full', 'sm']}>
+            <option value='st'>ST</option>
+            <option value='sc'>SC</option>
+            <option value='obc-a'>OBC-A</option>
+            <option value='obc-b'>OBC-B</option>
+            <option value='ur'>UR</option>
+          </Select>
+        </Stack>
+      </FormControl>
+      <ButtonGroup spacing='6'>
+        <Button>Reset</Button>
+        <Button colorScheme='blue'>Save</Button>
+      </ButtonGroup>
+    </VStack>
+  );
+}
+
+function AcademicDetailsForm() {
+  return <Box />;
+}
+function WorkExperienceForm() {
+  return <Box />;
+}
+function AdditionalDocumentsForm() {
+  return <Box />;
 }
 
 function AccountTab() {
