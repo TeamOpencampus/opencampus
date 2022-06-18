20c72c1bcb4ad08b400648bbdf46e4f8741fd028
Asim Bera <asimbera@outlook.in>
2022-06-18 17:06:54 +0530

    Redesign user's profile page and intgrate with backend (#74)
    Fixes #70

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import NiceModal from '@ebay/nice-modal-react';
+import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider, QueryFunction } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import App from './App';
@@ -14,16 +16,39 @@ const theme = extendTheme({
   },
 });
 
-const queryClient = new QueryClient();
+const defaultQueryFn: QueryFunction = ({ queryKey }) =>
+  axios
+    .get(queryKey.at(0)! as string, {
+      baseURL: import.meta.env.VITE_API_URL,
+      headers: {
+        Authorization: JSON.parse(localStorage.getItem('user')!).token,
+      },
+    })
+    .then((res) => res.data)
+    .catch((err) => {
+      if (axios.isAxiosError(err)) {
+        throw err;
+      }
+    });
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      queryFn: defaultQueryFn,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
         <ChakraProvider theme={theme}>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
+          <NiceModal.Provider>
+            <BrowserRouter>
+              <App />
+            </BrowserRouter>
+          </NiceModal.Provider>
         </ChakraProvider>
       </QueryClientProvider>
     </RecoilRoot>
